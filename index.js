83fd813e598bf7acf5c7044f97e6fa4b808c34a2
Newton Manyisa <manyisanewton26@gmail.com>
2024-11-27 23:49:27 +0300

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ let multiply = num1 * num2;// 31 * 2 = 62
 // Generate a random integer greater than 0
 let random = Math.floor(Math.random() * 100) + 1; 
 // Calculate the remainder to get 4
-let num3 = 16;
-let num4 = 6;
+let num3 = 24;
+let num4 = 5;
 let mod = num3 % num4; 
 
 // Find the highest number in a set
-let max= Math.max(7,6,12,20)
+let max= Math.max(7,9,2,12,20)
